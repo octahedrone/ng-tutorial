e456cd5f288b68c1563827986da855c0331fa7d3
Andrii Ailikov <octahedrone@gmail.com>
2022-05-16 21:18:42 +0300

    Add AdventureScriptStep.OrderNumber and migration

--- source/Db/AdventureScriptStep.cs
+++ source/Db/AdventureScriptStep.cs
@@ -5,6 +5,7 @@ public class AdventureScriptStep
     public int Id { get; set; }
     public int AdventureScriptId { get; set; }
     public int? ParentStepId { get; set; }
+    public int OrderNumber { get; set; }
     public string OptionText { get; set; }
     public string Text { get; set; }
 }
---
+++ source/Db/Migrations/20220516181644_OrderSteps.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Db.Migrations
+{
+    public partial class OrderSteps : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OrderNumber",
+                table: "AdventureScriptSteps",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrderNumber",
+                table: "AdventureScriptSteps");
+        }
+    }
+}
